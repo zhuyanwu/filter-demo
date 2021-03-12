ffdacc41584fa0241e189ee3bc8089e9acac21d8
zhuyanwu <ywzhup@isoftstone.com>
2021-03-13 01:50:20 +0800

    second

--- src/main/java/com/zyw/filterdemo/config/MyFilter.java
+++ src/main/java/com/zyw/filterdemo/config/MyFilter.java
@@ -1,6 +1,9 @@
 package com.zyw.filterdemo.config;
 
+import com.zyw.filterdemo.controller.CompanyController;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -9,8 +12,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
+@Component
 @WebFilter(filterName = "myFilter",urlPatterns = "/*")
 public class MyFilter implements Filter {
+
+    @Autowired
+    private CompanyController companyController;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         log.info("init...");
